6dcff4e667d07de988b0b74d7fc39098164b3374
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-05 00:51:02 -0300

    save commit

---
+++ src/main/java/br/com/cesarfcg/productscrud/ProductsCrudTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarfcg.productscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCrudTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsCrudTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cesarfcg/productscrud/ProductsCrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesarfcg.productscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsCrudTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
