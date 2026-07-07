71c106e47aa3d5abea08708d8154edd4ae1452fb
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-07 12:11:26 -0300

    Controller final

--- src/main/java/br/com/cesarfcg/productscrud/Controller/ProductController.java
+++ src/main/java/br/com/cesarfcg/productscrud/Controller/ProductController.java
@@ -1,15 +1,34 @@
 package br.com.cesarfcg.productscrud.Controller;
 
+import br.com.cesarfcg.productscrud.Entity.ProductEntity;
 import br.com.cesarfcg.productscrud.Service.ProductsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/products")
 public class ProductController {
     private ProductsService productsService;
 
     public ProductController(ProductsService productsService) {
         this.productsService = productsService;
+    }
+
+    @GetMapping("/list")
+    public List<ProductEntity> list(){
+        return productsService.list();
+    }
+    @PostMapping("/create")
+    public List<ProductEntity> create(@RequestBody ProductEntity productEntity){
+        return productsService.create(productEntity);
+    }
+    @PutMapping("/update")
+    public List<ProductEntity> update(@RequestBody ProductEntity productEntity){
+        return productsService.update(productEntity);
+    }
+    @DeleteMapping("/delete/{id}")
+    public List<ProductEntity> delete(@PathVariable("id")Long id ){
+        return productsService.delete(id);
     }
 }
