f4577eb1d449d1e195209d19c7b2aa8a3262ed6b
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-11 16:13:33 -0300

    Adicionando validações no Entity

--- src/main/java/br/com/cesarfcg/productscrud/Entity/ProductEntity.java
+++ src/main/java/br/com/cesarfcg/productscrud/Entity/ProductEntity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,8 +20,11 @@ public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotNull
     private String description;
+    @PositiveOrZero
     private Double price;
 
 }
